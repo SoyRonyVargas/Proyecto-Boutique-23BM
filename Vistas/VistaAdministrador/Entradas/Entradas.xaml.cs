b8692eed7cb2f9febf95e5dae8cc1f831b09c8fc
TreDogPlay <Martin.aglr@hotmail.com>
2023-07-25 06:53:32 -0600

    Entradas-Vista

---
+++ Vistas/VistaAdministrador/Entradas/Entradas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto23BMBoutique2.Vistas.VistaAdministrador.Entradas
+{
+    /// <summary>
+    /// Lógica de interacción para Entradas.xaml
+    /// </summary>
+    public partial class Entradas : Window
+    {
+        public Entradas()
+        {
+            InitializeComponent();
+        }
+    }
+}
